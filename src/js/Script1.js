247ac245f59b5e16bc77f44d937a4d4108e39de5
unknown <gonsarkey@gmail.com>
2025-02-07 16:26:34 -0600

    ejercicio

---
+++ src/js/Script1.js
@@ -0,0 +1,16 @@
+/* EJERCICIO 7
+Declara una variable y utiliza el operador ternario para determinar si una persona es mayor de edad o no,
+muestra el resultado en consola.
+*/
+
+//1. DECLARAR LA VARIABLE
+
+let edad = 52;
+
+//2. OPERADOR TERNARIO: se utiliza para simplificar el código cuando se necesita comprobar una condición
+
+let mensaje = (edad >= 18) ? "Es mayor de edad" : "No es mayor de edad";
+
+//3. MOSTRAR RESULTADO
+
+console.log(mensaje);
